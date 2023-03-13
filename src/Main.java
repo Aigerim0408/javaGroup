c6b8628ed926ea961af610e94845bbfe6d1f2641
beka3301 <bekturhanazarov98@gmail.com>
2023-03-13 20:40:33 +0600

    upgrage commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        Aidar aydar = new Aidar();
+        aydar.name = "Айдар";
+        aydar.surename = "Жапаров";
+        aydar.age = 18;
+        aydar.gender = "Male";
+        aydar.height = "180cm";
+        aydar.weight = "65kg";
+        aydar.character = "Позитивный";
+        System.out.println("Name: " + aydar.name + "\n" + "Surename: " + aydar.surename + "\n" + "Age: "
+                + aydar.age + "\n" + "Gender: " + aydar.gender + "\n" + "Height: " + aydar.height + "\n"
+                + "Weight" + aydar.weight + "\n" + "Character: " + aydar.character);
     }
 }
