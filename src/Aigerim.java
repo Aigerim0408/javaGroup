231da5d07d6cb7f43269a9d923d1ba09d0b66be9
Aigerim <a.zhamalbekova@gmail.com>
2023-03-14 21:23:59 +0600

    commit aigerim

--- src/Aigerim.java
+++ src/Aigerim.java
@@ -1,5 +1,6 @@
 public class Aigerim {
     String name;
+    int age;
     String dateOfBirth;
     String address;
     String education;
@@ -8,4 +9,55 @@ public class Aigerim {
     int workExperience;
     String hobby;
     String maritalStatus;
+
+    public Aigerim(String name, int age, String dateOfBirth, String address, String education, String specialty, String currentWorkPlace, int workExperience, String hobby, String maritalStatus) {
+        this.name = name;
+        this.age = age;
+        this.dateOfBirth = dateOfBirth;
+        this.address = address;
+        this.education = education;
+        this.specialty = specialty;
+        this.currentWorkPlace = currentWorkPlace;
+        this.workExperience = workExperience;
+        this.hobby = hobby;
+        this.maritalStatus = maritalStatus;
+    }
+
+    public Aigerim() {
+    }
+
+    @Override
+    public String toString() {
+        return  "Aigerim{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", dateOfBirth='" + dateOfBirth + '\'' +
+                ", address='" + address + '\'' +
+                ", education='" + education + '\'' +
+                ", specialty='" + specialty + '\'' +
+                ", currentWorkPlace='" + currentWorkPlace + '\'' +
+                ", workExperience=" + workExperience +
+                ", hobby='" + hobby + '\'' +
+                ", maritalStatus='" + maritalStatus + '\'' +
+                '}';
+    }
+
+    public void method() {
+        if (age <= 32) {
+            System.out.println("name='" + name + '\'' +
+                    ", age=" + age +
+                    ", dateOfBirth='" + dateOfBirth + '\'' +
+                    ", address='" + address + '\'' +
+                    ", education='" + education + '\'' +
+                    ", specialty='" + specialty + '\'' +
+                    ", currentWorkPlace='" + currentWorkPlace + '\'' +
+                    ", workExperience=" + workExperience +
+                    ", hobby='" + hobby + '\'' +
+                    ", maritalStatus='" + maritalStatus + '\'' +
+                    '}');
+        } else {
+            System.out.println("taktalsyn!");
+        }
+    }
 }
+
--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ public class Main {
         // Aigerim
         Aigerim person = new Aigerim();
         person.name = "Aigerim";
+        person.age = 32;
         person.dateOfBirth = "04.08.1990";
         person.address = "Pioneer Street 38";
         person.hobby = "Travelling all over the word";
@@ -12,11 +13,7 @@ public class Main {
         person.workExperience = 8;
         person.maritalStatus = "Single";
         System.out.println("===================");
-        System.out.println("Name: " + person.name + "\n"+"Date of birth: " + person.dateOfBirth +"\n"
-                + "Address: " + person.address +"\n"+ "Hobby: " + person.hobby+ "\n"+ "Education: "
-                + person.education + "\n"+"Specialty: " + person.specialty +"\n"+
-                "Current work place: " + person.currentWorkPlace +"\n"+ "Work experience: " + person.workExperience
-                +" years" + "\n" +"Marital status: " + person.maritalStatus);
+        person.method();
 
         System.out.println("===================");
 
