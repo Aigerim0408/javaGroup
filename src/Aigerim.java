ff8aba08f543143401031d25520433b78adbccda
Aigerim <a.zhamalbekova@gmail.com>
2023-03-15 11:27:47 +0600

    commit aigerim

--- src/Aigerim.java
+++ src/Aigerim.java
@@ -10,24 +10,6 @@ public class Aigerim {
     String hobby;
     String maritalStatus;
 
-    public Aigerim(String name, int age, String dateOfBirth, String address, String education,
-                   String specialty, String currentWorkPlace, int workExperience, String hobby,
-                   String maritalStatus) {
-        this.name = name;
-        this.age = age;
-        this.dateOfBirth = dateOfBirth;
-        this.address = address;
-        this.education = education;
-        this.specialty = specialty;
-        this.currentWorkPlace = currentWorkPlace;
-        this.workExperience = workExperience;
-        this.hobby = hobby;
-        this.maritalStatus = maritalStatus;
-    }
-
-    public Aigerim() {
-    }
-
     public void method() {
         if (age <= 32) {
             System.out.println("name: " + name + "\n" +
